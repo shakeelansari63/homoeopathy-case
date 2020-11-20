ebd4d255d2cbfee35fb5bcbe6a989709a0ce38bb
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2020-11-20 21:24:41 +0530

    Updated for Auto compile

--- compile.py
+++ compile.py
@@ -1,25 +1,46 @@
 from py_compile import compile
 from PyInstaller.__main__ import run as pybuild
 import os
+import sys
+import shutil
+
+# Cleanup existing build and dist dirs
+if os.path.exists('build/'):
+    shutil.rmtree('build/')
+
+if os.path.exists('dist/'):
+    shutil.rmtree('dist/')
+
+if os.path.exists('__pycache__/'):
+    shutil.rmtree('__pycache__/')
 
 for pyfile in os.listdir('MainApp/'):
     if pyfile.endswith('.py'):
         print(pyfile)
-        compile('MainApp/' + pyfile, 'MainAppC/' + pyfile + 'c')
+        compile('MainApp/' + pyfile, 'pycs/' + pyfile + 'c')
 
 # For importing qtmodern theme
 import qtmodern
 qtm_path = os.path.dirname(qtmodern.__file__)
+
+# Set Logo File
+if sys.platform == 'linux' or sys.platform == 'darwin':
+    logofile = "logo.svg"
+else:
+    logofile = "logo.ico"
 
 pybuild([
     "main.py",
     "--clean",
     "--onedir",
     "--name=HomoeopathyCase",
-    "--add-data=./MainAppC:./MainApp",
+    "--add-data=./pycs:./MainApp",
     "--add-data=./MainApp/img:./MainApp/img",
     "--add-data=./MainApp/resources:./MainApp/resources",
     "--add-data={}:./qtmodern".format(qtm_path),
     "--windowed",
-    "--icon=logo.icon"
+    "--icon={}".format(logofile)
 ])
+
+# Cleanup Temporary Files
+shutil.rmtree('pycs/')
