c6fe3767b16d67ced660a20c3a4148505bfe4a06
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2021-08-29 23:42:00 +0530

    Display Patients in Cards

--- src/app/services/patients-data.service.ts
+++ src/app/services/patients-data.service.ts
@@ -3,10 +3,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Patient } from '../models/patient'
 import notify from 'devextreme/ui/notify'
 import { Store } from '@ngrx/store';
-import { 
-  addPatient, 
+import {
+  addPatient,
   replacePatientList,
-  hideNewPatientForm 
+  hideNewPatientForm
 } from '../state/patient/patient.actions'
 import { Subject, Observable } from 'rxjs';
 
@@ -17,25 +17,29 @@ export class PatientsDataService {
 
   baseUrl: string = 'http://localhost:3000'
   patientsUrl: string = `${this.baseUrl}/patients`
-  hdr: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json'})
+  hdr: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })
 
   constructor(private http: HttpClient, private store: Store) {
-   }
+  }
 
   newPatient(pat: Patient) {
-    this.http.post<Patient>(this.patientsUrl, pat, {headers: this.hdr}).forEach( result => {
+    this.http.post<Patient>(this.patientsUrl, pat, { headers: this.hdr }).forEach(result => {
       this.store.dispatch(hideNewPatientForm())
 
       // Notify User for patient creating
       notify('New Patient Created')
     })
-    
-    this.store.dispatch(addPatient({patient: pat}))
+
+    this.store.dispatch(addPatient({ patient: pat }))
   }
 
   getAllPatients() {
-    this.http.get<Patient[]>(this.patientsUrl).forEach( patients => {
-      this.store.dispatch(replacePatientList({patientList: patients}))
+    this.http.get<Patient[]>(this.patientsUrl).forEach(patients => {
+      // Create Sorted List in descending
+      const patListSorted = patients.sort((pat1, pat2) => pat1.id > pat2.id ? -1 : 1)
+
+      // Update State
+      this.store.dispatch(replacePatientList({ patientList: patListSorted }))
     })
   }
 }
