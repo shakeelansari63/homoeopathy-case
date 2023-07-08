9efe301d64f34088cd185432e3c14a891d379046
Shakeel Ansari <Shakeel.Ansari@Gmail.com>
2023-07-09 02:26:07 +0530

    Add better sqlite for electron

---
+++ public/models/db.js
@@ -0,0 +1,2 @@
+const sqlite = require("better-sqlite3-with-prebuilds")
+const db = new sqlite("")
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App(){
       <button onClick={()=>{
          ipcRenderer.send('async-test', 'ping')
          // reply
-         ipcRenderer.on('asynchronous-reply', (event, arg) => {
+         ipcRenderer.on('async-reply', (event, arg) => {
          console.log("Hiii",arg) // prints "Hiii pong"
          })
      }}>Async</button>
